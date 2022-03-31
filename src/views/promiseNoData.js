24c0b3c4774334741be9259c1c4ba59914fe53de
Adam <adam.cerven@gmail.com>
2022-03-31 16:13:42 +0200

    Loader finished

--- src/views/promiseNoData.js
+++ src/views/promiseNoData.js
@@ -1,10 +1,12 @@
+import Loading from "../components/loading";
+
 export default function promiseNoData(promiseState) {
  
     if(!promiseState.promise) {
         return <div>No data</div>
     }
     if(!promiseState.data && !promiseState.error) {
-        return <img src="../../images/acloader.gif" alt="" className="loadingIcon" />
+        return <Loading />
     }  
     if(!promiseState.data && promiseState.error) {
         return <div className="debug" >{promiseState.error}</div>
