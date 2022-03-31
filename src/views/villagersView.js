f0e4fa909e13b9d11e65e94415cb08ff8076ce3d
Thelarion <46968231+Thelarion@users.noreply.github.com>
2022-03-31 20:36:05 +0200

    Styling villagers

--- src/views/villagersView.js
+++ src/views/villagersView.js
@@ -7,9 +7,9 @@ function VillagersView(props){
             }
             return (
                 <div className="list__col" key={"villagers_"+singleResult.id} onClick={() => itemClicked()}>
-                    <div className="listItem" >
-                        <img className="listItem__image" alt="" src={singleResult.icon_uri}/>
-                        <div className="listItem__text">
+                    <div className="listItem__villagers" >
+                        <img className="listItem__image_villagers" alt="" src={singleResult.icon_uri}/>
+                        <div className="listItem__text_villagers">
                             {singleResult.name["name-EUen"]}
                         </div>
                     </div>
