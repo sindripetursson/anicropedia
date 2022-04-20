1554f419e5101233ea1fcc038daac0936b0b8b93
sindripetursson <sindripet@gmail.com>
2022-04-20 14:52:20 +0200

    homescreen is now epic

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -4,45 +4,47 @@ import { Link } from "react-router-dom";
 export default 
 function HomeView(props) {
   return (
-    <div className="home">
-            <div className='home__row'>
-                <div className="home__box" >
-                    <Link className="home__link" to="/island">
-                        <img className="home__image" alt="" src={"../../images/homeIcons/myIsland.png"}/>
-                        <h3 className="home__text"> My Island </h3>
+    <div className="list">
+        <div className="list__container">
+            <div className='list__row__home'>
+                <div className="list__col__home" >
+                    <Link className="listItem__home" to="/island">
+                        <img className="listItem__image__home" alt="" src={"../../images/homeIcons/myIsland.png"}/>
+                        <h3 className="listItem__text__home"> My Island </h3>
                     </Link>
                 </div>
-                <div className="home__box" >
-                    <Link className="home__link" to="/encyclopedia">
-                        <img className="home__image" alt="" src={"../../images/homeIcons/encyclopedia.png"}/>
-                        <h3 className="home__text"> Encyclopedia </h3>
+                <div className="list__col__home" >
+                    <Link className="listItem__home" to="/encyclopedia">
+                        <img className="listItem__image__home" alt="" src={"../../images/homeIcons/encyclopedia.png"}/>
+                        <h3 className="listItem__text__villagers"> Encyclopedia </h3>
                     </Link>
                 </div>
-                <div className="home__box" >
-                    <Link className="home__link" to="/villagers">
-                        <img className="home__image" alt="" src={"../../images/homeIcons/villagers.png"}/>
-                        <h3 className="home__text"> Villagers </h3>
+                <div className="list__col__home" >
+                    <Link className="listItem__home" to="/villagers">
+                        <img className="listItem__image__home" alt="" src={"../../images/homeIcons/villagers.png"}/>
+                        <h3 className="listItem__text__villagers"> Villagers </h3>
                     </Link>
                 </div>
-                <div className="home__box" >
-                    <Link className="home__link" to="/music">
-                        <img className="home__image" alt="" src={"../../images/homeIcons/music.png"}/>
-                        <h3 className="home__text"> Music </h3>
+                <div className="list__col__home" >
+                    <Link className="listItem__home" to="/music">
+                        <img className="listItem__image__home" alt="" src={"../../images/homeIcons/music.png"}/>
+                        <h3 className="listItem__text__villagers"> Music </h3>
                     </Link>
                 </div>
-                <div className="home__box" >
-                    <Link className="home__link" to="/collectibles">
-                        <img className="home__image" alt="" src={"../../images/homeIcons/collectibles.png"}/>
-                        <h3 className="home__text"> Collectibles </h3>
+                <div className="list__col__home" >
+                    <Link className="listItem__home" to="/collectibles">
+                        <img className="listItem__image__home" alt="" src={"../../images/homeIcons/collectibles.png"}/>
+                        <h3 className="listItem__text__villagers"> Collectibles </h3>
                     </Link>
                 </div>
-                <div className="home__box" >
-                    <Link className="home__link" to="/info">
-                        <img className="home__image" alt="" src={"../../images/homeIcons/help.png"}/>
-                        <h3 className="home__text"> Info </h3>
+                <div className="list__col__home" >
+                    <Link className="listItem__home" to="/info">
+                        <img className="listItem__image__home" alt="" src={"../../images/homeIcons/help.png"}/>
+                        <h3 className="listItem__text__villagers"> Info </h3>
                     </Link>
                 </div>
             </div>
+        </div>
     </div>
   )
 }
