76452980b17078544075e699da9b00aa9054a8d5
sindripetursson <sindripet@gmail.com>
2022-05-01 21:17:12 +0200

    island page style progress

--- src/views/collectibleView.js
+++ src/views/collectibleView.js
@@ -14,11 +14,14 @@ function CollectibleView(props){
             return (
                 <div className={props.islandView ? "list__col__encyclopedia--island" : "list__col__encyclopedia"} key={"collectible_" + singleResult['file-name']} onClick={() => itemClicked()}>
                     <div className="listItem__encyclopedia" >
-                        <img className="listItem__image__encyclopedia" alt="" src={singleResult.image_uri}/>
-                        <div className="listItem__text__encyclopedia">
+                        <img className={props.islandView ? "listItem__image__encyclopedia--island" : "listItem__image__encyclopedia"} alt="" src={singleResult.image_uri}/>
+                        <div className={props.islandView ? "listItem__text__encyclopedia--island" : "listItem__text__encyclopedia"}>
                             {capitalizeFirstLetter(singleResult.name["name-EUen"])}
                         </div>
+                        {props.islandView ?
+                        <></> :
                         <img className={isItemInCollection(singleResult, props.category, false, props.userModel) ? "checkmark" : "hidden"} alt="checkmark" src="../../images/inCollection.svg"/>
+                        }
                     </div>
                 </div>
             );
@@ -29,7 +32,7 @@ function CollectibleView(props){
     return(
         <div className="list">
             <div className={props.islandView ? "list__container__encyclopedia--island" : "list__container__encyclopedia"}>
-                <div className="list__row__encyclopedia">
+                <div className={props.islandView ? "list__row__encyclopedia--island" : "list__row__encyclopedia"}>
                     {renderData(props.data)}
                 </div>
             </div>
