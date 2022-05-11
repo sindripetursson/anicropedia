fe86f91616d50afc5e0092350b46a74dfd04169e
Kári Steinn Aðalsteinsson <kari_steinn@outlook.com>
2022-05-12 00:06:04 +0200

    Added session check to info page

--- src/presenters/infoPresenter.js
+++ src/presenters/infoPresenter.js
@@ -1,12 +1,13 @@
 import React from "react";
 import InfoView from '../views/infoView';
+import { sessionCheck } from "../utils";
 
 export default
 function Info(props) {
     React.useEffect(() => {
         props.setDetailsOn(false);
     })
-    return (
+    return sessionCheck() || (
         <InfoView/>
     );
 }
